c20bfaffdda071e8386f09d46efb2c23603e88d3
Dickson <jdickson@agoda.local>
2018-10-19 23:35:01 +0700

    convert to vs2017 proj

--- Obvs.Kafka.Tests/Properties/AssemblyInfo.cs
+++ Obvs.Kafka.Tests/Properties/AssemblyInfo.cs
@@ -1,15 +1,6 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Obvs.Kafka.Tests")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("megakid")]
-[assembly: AssemblyProduct("Obvs.Kafka.Tests")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0875b0d8-bf3f-4b60-a811-72a95d8af592")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
